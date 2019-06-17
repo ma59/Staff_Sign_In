f49d6066dfb3d3f2869fdc64d4f08e02f9fdb5e6
ma59 <51910647+ma59@users.noreply.github.com>
2019-06-17 11:37:49 +0100

    Created class staff

---
+++ Staff.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Staff_sign_in
+{
+    class Staff
+    {
+        public string Person();
+        {
+            
+        }
+    }
+}
